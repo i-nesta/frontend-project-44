fb6e8404c3929beca4ba39aa16d00ea541a28314
i-nesta <me@nestapro.ru>
2023-04-18 18:24:55 +0300

    step5finalwork

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 import welcome from '../src/cli.js';
+import mainGame from './brain-even.js';
 
 console.log('Welcome to the Brain Games!');
-console.log(`Hello, ${welcome()}`);
+const name = welcome();
+console.log(`Hello, ${name}`);
+mainGame(name);
